bfafb8997d49621e339ba91f7fc4cb3abea91b6a
MoCkZeTa <139861240+MoCkZeTa@users.noreply.github.com>
2024-10-12 14:09:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,17 +12,10 @@ const authentication=require('./middleware/authentication');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.set('trust proxy', 1);
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-  })
-);
+
+
 app.use(express.json());
-app.use(xss());
-app.use(cors());
-app.use(helmet());
+
 
 
 // routes
