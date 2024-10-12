de6e3973708d70e6518eda2231ec4f9ca92963b5
MoCkZeTa <139861240+MoCkZeTa@users.noreply.github.com>
2024-10-12 14:47:42 +0530

    Update jobs.js

--- routes/jobs.js
+++ routes/jobs.js
@@ -8,11 +8,9 @@ const {
   getAllJobs,
   updateJob,
   getJob,
-  showStats
 } = require("../controllers/jobs");
 
 router.route("/").post(createJob).get(getAllJobs);
-router.route('/stats').get(showStats);
 
 router.route("/:id").get(getJob).delete(deleteJob).patch(updateJob);
 
